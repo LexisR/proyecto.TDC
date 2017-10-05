8dc4aec725847596ce05c823f7b27e9659c67f20
LexisR <lexisrodriguez22@gmail.com>
2017-10-05 17:17:24 -0300

    cambios correcciones

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function isValidCard (){// esta funcion debe validar si el n° de tarjeta es valida
   var consultaTdc = prompt("Hola introduzca su numero de tarjeta"); // consultar el numero de tdc
   var numerosPares = []; // cramos un array para guardar solo los numeros en posicion par de tarjeta invertida
@@ -11,36 +10,38 @@ function isValidCard (){// esta funcion debe validar si el n° de tarjeta es val
        numerosImpares.push(parseInt(consultaTdc[i]));
    }
   }
+
   //Parte 2: Recorrer numeros pares, cada elemento multiplicar por 2 y si > 10 sumar digitos y resulatdo colocarlo en posicion actual
   for (var j = 0; j <= numerosPares.length -1; j++){
     var multiplicacion = numerosPares[j] * 2;  // cplocar el nmbre d ela variable para acceder
     if (multiplicacion >= 10 ){
       // sumar digitos, como saco los digitos de un numero?
-    numerosPares = (multiplicacion % 10) + (multiplicacion/10);
+    numerosPares[j] = parseInt((multiplicacion % 10) + (multiplicacion/10));
+   } else {
+     numerosPares[j] = multiplicacion;
    }
 
- }
+}
+
   // parte 3: debo sumar los numeros pares y los numeros impares y de ese resultado debo obtebere el residuo de la division entre 10
   var sumaPar = 0;
   for (var k = 0; k < numerosPares.length; k++){
     sumaPar = sumaPar + numerosPares[k];
   }
 
-  var sumaImpar = 0;
+ var sumaImpar = 0;
   for (var l = 0; l < numerosImpares.length; l++){
     sumaImpar = sumaImpar + numerosImpares[l];
   }
 
-  var resultado = sumaPar + sumaImpar;
+ var resultado = sumaPar + sumaImpar;
+
     if (resultado % 10 === 0){
-      return document.write ("Tu numero de tarjeta es valido, Bienvenido a mi web");
+      return alert ("Tu numero de tarjeta es valido, Bienvenido a mi web");
     }else {
       return alert("Estimado usuario, su  numero de tarjeta es invalido, por ingrese un numero de tarjeta valido");
     }
 
-
 }
 
-
-
-isValidCard (); // no puedo invocar variables SOLO SE PUEDEN INVOCAR LAS FUNCIONES
+isValidCard ()
